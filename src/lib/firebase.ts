673139627d5ca79e59399b8a68e9d86361409504
Daniel Buck <danbuck@Daniels-MacBook-Air.local>
2025-12-04 07:50:56 -0300

    fix: netlify deploy config and secure firebase keys

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -3,13 +3,13 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCeDUiqykB_0wsxFWFq9YnL_-s1aom1Q78",
-    authDomain: "pb-hiit.firebaseapp.com",
-    projectId: "pb-hiit",
-    storageBucket: "pb-hiit.firebasestorage.app",
-    messagingSenderId: "231248116598",
-    appId: "1:231248116598:web:c50d9fe9d0a34f6d4d9857",
-    measurementId: "G-BTML2MRQ1J"
+    apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+    authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+    projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+    storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+    appId: import.meta.env.VITE_FIREBASE_APP_ID,
+    measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 };
 
 const app = initializeApp(firebaseConfig);
